15a817f9b61f422813e7c3d06f2e215f2f9c777d
Jaroslav Barton <jbarton@vendavo.com>
2012-10-16 23:49:10 +0200

    Added link to github -> new version 5.1.22.1

--- MysqlConnectorjGrailsPlugin.groovy
+++ MysqlConnectorjGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class MysqlConnectorjGrailsPlugin {
-    def version = "5.1.22"
+    def version = "5.1.22.1"
     def grailsVersion = "1.2.0 > *"
     def dependsOn = [:]
     def pluginExcludes = [
@@ -11,4 +11,5 @@ class MysqlConnectorjGrailsPlugin {
     def title = "MySQL Connector/J"
     def description = "MySQL Connector/J"
     def documentation = "http://grails.org/plugin/mysql-connectorj"
+    def scm = [ url: "https://github.com/djaara/mysql-connectorj" ]
 }
